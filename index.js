ef7cea266f232f294e57d2dd078fa713c8cb754c
Francis Bourassa <francis.bourassa@play-lu.com>
2023-03-16 12:45:00 -0400

    pseudocode

--- index.js
+++ index.js
@@ -1,37 +1,77 @@
 import { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({ port: 3232 });
+// Device types
+const LU_SOURCE_TYPE = "lu";
+const MARBOTIC_SOURCE_TYPE = "marbotic";
+
+// Message types
+const SET_SOURCE_TYPE_MESSAGE_TYPE = "set_source";
+const START_GAME_MESSAGE_TYPE = "start_game";
+const END_GAME_MESSAGE_TYPE = "end_game";
+
+// Game states
+const GAME_STATE_PENDING = "pending";
+const GAME_STATE_STARTED = "started";
+
+// Statuses
+let gameId = null;
+let gameState = GAME_STATE_PENDING;
+let gameAnswers = [];
+
+let luDeviceIds = [];
+let marboticDeviceIds = [];
+
+const wss = new WebSocketServer({ port: 2222 });
 
 wss.on("connection", (ws) => {
-  ws.send("Welcome to the chat, enjoy :)");
+  ws.send("Hey, welcome to the Server. Enjoy");
 
   ws.on("message", (data) => {
     let message;
 
-    console.log(">>>>>>", data);
-
     try {
       message = JSON.parse(data);
-      å;
     } catch (e) {
       sendError(ws, "Wrong format");
-
       return;
-    }
-
-    if (message.type === "NEW_MESSAGE") {
-      wss.clients.forEach((client) => {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(data);
-        }
-      });
     }
   });
 });
 
-const sendError = (ws, message) => {
-  console.log(ws, message);
+const LuChannelHandler = () => {
+  function Handle(message) {
+    if (message.type === START_GAME_MESSAGE_TYPE) {
+      if (gameState === GAME_STATE_PENDING) {
+        gameState = GAME_STATE_STARTED;
+        gameId = message.data.gameId;
+      } else {
+        // ERROR, game already started
+      }
+    } else if (message.type === END_GAME_MESSAGE_TYPE) {
+      if (gameState === GAME_STATE_STARTED) {
+        gameState = GAME_STATE_PENDING;
+        gameId = null;
 
+        // Decide winner
+
+        // Dispatch winner to Lu
+        // wss.clients.forEach((client) => {
+        //   if (client !== ws && client.readyState === WebSocket.OPEN) {
+        //     client.send(data);
+        //   }
+        // });
+      } else {
+        // ERROR, game already started
+      }
+    }
+  }
+};
+
+const MarboticChannelHandler = () => {
+  function Handle(message) {}
+};
+
+const sendError = (ws, message) => {
   const messageObject = {
     type: "ERROR",
     payload: message,
